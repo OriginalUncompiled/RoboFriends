9519b4d25f6897a2c1c99fd0cebcd37d1dcf3064
carstendold1@gmail.com <88604752+OriginalUncompiled@users.noreply.github.com>
2024-06-17 17:32:26 +0200

    added redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,45 +1,56 @@
-import React, { useState, useEffect } from 'react';
+import { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
-const App = () => {
+import { setSearchField, requestRobots } from '../actions.js';
 
-    const [robots, setRobots] = useState([]);
-    const [searchfield, setSearchfield] = useState('');
-
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => setRobots(users))
-    }, []);
-
-    const onSearchChange = (event) => {
-        setSearchfield(event.target.value);
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
     }
-
-    const filteredRobots = robots.filter(robot => {
-        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
-    return !robots.length ? // if robots.length = 0
-    <h1 className='tc'>Loading...</h1> :
-    <div className='tc' style={{height: '100vh'}}>
-        <div style={{height: '18vh'}}>
-            <h1>RoboFriends</h1>
-            <SearchBox searchChange={onSearchChange} />
-        </div>
-        <Scroll style={{height: '80vh'}}>
-            <ErrorBoundary>
-                {!filteredRobots.length ? ( // if filteredRobots.length = 0
-                    <h2 className='tc f2 pa5 nonCardH2'>Can't find a robot with '{searchfield}' in their name...</h2>
-                    ) : ( // else
-                    <CardList robots={filteredRobots} />
-                )}
-            </ErrorBoundary>
-        </Scroll>
-    </div>
+}
+const mapDispatchToProps = dispatch => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
+    }
 }
 
-export default App;
+class App extends Component {
+    componentDidMount() {
+        this.props.onRequestRobots();
+    };
+
+    render() {
+        const { searchField, onSearchChange, robots, isPending, error } = this.props;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        })
+        return isPending ? // if robots.length = 0
+        <h1 className='tc'>Loading...</h1> :
+        <div className='tc' style={{height: '100vh'}}>
+            <div style={{height: '18vh'}}>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={onSearchChange} />
+            </div>
+            <Scroll style={{height: '80vh'}}>
+                <ErrorBoundary>
+                    {!robots.length && !error ? ( // if filteredRobots.length = 0
+                        <h2 className='tc f2 pa5 nonCardH2'>Can't find a robot with '{searchField}' in their name...</h2>
+                        ) : ( // else
+                        <CardList robots={filteredRobots} />
+                    )}
+                </ErrorBoundary>
+            </Scroll>
+        </div>
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { legacy_createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import { thunk } from 'redux-thunk';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import 'tachyons';
 import App from './containers/App';
+import { searchRobots, requestRobots } from './reducers.js';
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = legacy_createStore(rootReducer, applyMiddleware(thunk, logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App/>
+		</Provider>
 	</React.StrictMode>
 );
 
